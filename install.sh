4cfca86992a765c10ba7b089d9db35175ac756b0
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-04-06 12:07:29 +0900

    feat: Install fish with GitHub CLI

--- install.sh
+++ install.sh
@@ -4,22 +4,11 @@
 
 set -euo pipefail
 
-readonly arg="${1:-}"
+cd "$(mktemp -d)"
 
-if [[ -n "${arg}" ]]; then
-  readonly tag="${arg}"
-  if ! [[ "${tag}" =~ ^[0-9]+\.[0-9]+[.b][0-9]+$ || "${tag}" =~ ^OpenBeta_r[12]$ ]]; then
-    echo "Invalid tag: ${tag}" >&2
-    exit 1
-  fi
-else
-  tag="$(curl -fsS 'https://api.github.com/repos/fish-shell/fish-shell/releases/latest' | jq -r '.tag_name')"
-  readonly tag
-fi
-
-readonly url="https://github.com/fish-shell/fish-shell/releases/download/${tag}/fish-${tag}-linux-x86_64.tar.xz"
+gh release -R fish-shell/fish-shell download "${1:-}" -p 'fish-*-linux-x86_64.tar.xz'
 
 readonly dest="${HOME}/.local/bin"
 test -d "${dest}" || mkdir -p "${dest}"
 
-curl -fsSL "${url}" | tar -xJ --directory="${dest}"
+tar -xf fish-*-linux-x86_64.tar.xz -C "${dest}"
